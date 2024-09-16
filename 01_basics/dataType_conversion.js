32ced390ebcdb8bec5967671785e1f0603bf0240
April27Dev <135101051+April27Dev@users.noreply.github.com>
2024-09-16 12:53:07 +0000

    day 2

---
+++ 01_basics/dataType_conversion.js
@@ -0,0 +1,15 @@
+let score="100"
+let id="3abc" 
+console.log(typeof id)//string
+let value=Number(score);
+console.log(typeof value) //number
+console.log(typeof(Number(id)))//number
+console.log(value)//100
+console.log(Number(id))//NaN
+let value=3
+let neg=-value//-3
+let power=2**3 //8
+console.log("1"+2)//12
+console.log("1" + 2 +3)//123
+console.log(2+3+"1")//51
+console.log(+true)//1
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,11 @@
+/*# "use strict";   treat all code as newer version of JS
+alert(3+3) =>error-using nodejs not browser
+null datatype-standalone value:- let state=null;
+undefined datatype :- let num;(no value given)
+symbol datatype:- used for uniqifying:)
+*/
+console.log(typeof "Vartika") //string
+          //or
+console.log(typeof ("Vartika")) //string
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
